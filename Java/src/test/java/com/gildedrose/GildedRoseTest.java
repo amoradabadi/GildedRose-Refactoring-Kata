8b4a6d3048324677466b58e06ac8d224d217f376
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-07 13:11:31 +0200

    add test for Quality should not be negative

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,38 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void whenSellHasPassed_shouldDegradesTwiceAsFast() {
+        Item[] items = new Item[]{new Item("foo", 1, 5)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void whenUpdatingQuality_shouldNotBeNegative() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality >= 0);
     }
 
 }

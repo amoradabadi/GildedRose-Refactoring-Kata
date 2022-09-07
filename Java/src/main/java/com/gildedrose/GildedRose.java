10ac3a87a2390b8f058e99e4e89b951b4d9f3b2a
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-07 14:37:42 +0200

    Implement and separate apply methods

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,56 +7,169 @@ class GildedRose {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+    static abstract class MyItem extends Item {
+        public MyItem(String name, int sellIn, int quality) {
+            super(name, sellIn, quality);
+        }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+        abstract Item apply();
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+    static class MyItemFactory {
+        public static MyItem getItem(Item item) {
+            return switch (item.name) {
+                case "Aged Brie" -> new AgedBrieItem(item);
+                case "Backstage passes to a TAFKAL80ETC concert" -> new BackstageItem(item);
+                case "Sulfuras, Hand of Ragnaros" -> new SulfurItem(item);
+                default -> new OtherItem(item);
+            };
         }
     }
+
+    static class AgedBrieItem extends MyItem {
+        public AgedBrieItem(Item item) {
+            super(item.name, item.sellIn, item.quality);
+        }
+
+        @Override
+        Item apply() {
+            if (this.quality < 50) {
+                this.quality += 1;
+            }
+
+            this.sellIn -= 1;
+
+            if (this.sellIn < 0) {
+                if (this.quality < 50) {
+                    this.quality += 1;
+                }
+            }
+            return this;
+        }
+    }
+
+    static class BackstageItem extends MyItem {
+        public BackstageItem(Item item) {
+            super(item.name, item.sellIn, item.quality);
+        }
+
+        @Override
+        Item apply() {
+            if (this.quality < 50) {
+                this.quality += 1;
+
+                if (this.sellIn < 11) {
+                    if (this.quality < 50) {
+                        this.quality += 1;
+                    }
+                }
+
+                if (this.sellIn < 6) {
+                    if (this.quality < 50) {
+                        this.quality += 1;
+                    }
+                }
+            }
+
+            this.sellIn -= 1;
+
+            if (this.sellIn < 0) {
+                this.quality = 0;
+            }
+            return this;
+        }
+    }
+
+    static class SulfurItem extends MyItem {
+        public SulfurItem(Item item) {
+            super(item.name, item.sellIn, item.quality);
+        }
+
+        @Override
+        Item apply() {
+            // Being a legendary item, never has to be sold or decreases in Quality
+            return this;
+        }
+    }
+
+    static class OtherItem extends MyItem {
+        public OtherItem(Item item) {
+            super(item.name, item.sellIn, item.quality);
+        }
+
+        @Override
+        Item apply() {
+            if (this.quality > 0) {
+                this.quality -= 1;
+            }
+
+            this.sellIn -= 1;
+
+            if (this.sellIn < 0) {
+                if (this.quality > 0) {
+                    this.quality -= 1;
+                }
+            }
+            return this;
+        }
+    }
+
+    public void updateQuality() {
+        for (Item item : items) {
+            MyItem myItem = MyItemFactory.getItem(item);
+            Item newItem = myItem.apply();
+            item.quality = newItem.quality;
+            item.sellIn = newItem.sellIn;
+//            if (!(item.name.equals("Aged Brie")
+//                || item.name.equals("Backstage passes to a TAFKAL80ETC concert"))) {
+//                if (item.quality > 0) {
+//                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                        item.quality = item.quality - 1;
+//                    }
+//                }
+//            } else {
+//                if (item.quality < 50) {
+//                    item.quality = item.quality + 1;
+//
+//                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                        if (item.sellIn < 11) {
+//                            if (item.quality < 50) {
+//                                item.quality = item.quality + 1;
+//                            }
+//                        }
+//
+//                        if (item.sellIn < 6) {
+//                            if (item.quality < 50) {
+//                                item.quality = item.quality + 1;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+
+//            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                item.sellIn = item.sellIn - 1;
+//            }
+
+//            if (item.sellIn < 0) {
+//                if (!item.name.equals("Aged Brie")) {
+//                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                        if (item.quality > 0) {
+//                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                                item.quality = item.quality - 1;
+//                            }
+//                        }
+//                    } else {
+//                        item.quality = 0;
+//                    }
+//                } else {
+//                    if (item.quality < 50) {
+//                        item.quality = item.quality + 1;
+//                    }
+//                }
+//            }
+        }
+    }
+
+
 }
